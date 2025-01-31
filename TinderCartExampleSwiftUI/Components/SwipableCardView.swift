73df710376f556987e17781a01510b5bda1c5336
fumiyasac <just1factory@gmail.com>
2025-01-31 21:44:06 +0900

    View要素の移動＆整理

---
+++ TinderCartExampleSwiftUI/Components/SwipableCardView.swift
@@ -0,0 +1,17 @@
+//
+//  SwipableCardView.swift
+//  TinderCartExampleSwiftUI
+//
+//  Created by 酒井文也 on 2025/01/31.
+//
+
+import SwiftUI
+
+struct SwipableCardView: View {
+
+    // MARK: - Body
+
+    var body: some View {
+        Text("SwipableCardView")
+    }
+}
